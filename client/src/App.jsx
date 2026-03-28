60014830312a5332cd6c380784e4e8785326983b
oltinios <80402091+oltinios@users.noreply.github.com>
2026-03-28 18:02:11 +0000

    Created the website

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+// client/src/App.jsx
+import React from "react";
+import "./App.css"; // <-- import your CSS file
+
+
+
+export default function App() {
+  return (
+    <div>
+      <h1>Film Rating Visualiser</h1>
+      <p>This is where your app starts.</p>
+    </div>
+  );
+}
